94d06bda161ddd543a1e4d7799ee4d6abc3ab6cc
maher moussa <maher.moussa90@yahoo.fr>
2026-05-16 21:49:26 +0200

    Update SecurityConfig.java

--- src/main/java/com/lebonplan/config/SecurityConfig.java
+++ src/main/java/com/lebonplan/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final UserDetailsService userDetailsService;
 
-    @Value("${app.cors.allowed-origins:http://localhost:5173,http://localhost:3000,https://lebonplan2.netlify.app/,https://lesbonsplans.netlify.app/,https://lesbonplan.vercel.app/}")
+    @Value("${app.cors.allowed-origins:http://localhost:5173,http://localhost:3000,https://lebonplan2.netlify.app/,https://lesbonsplans.netlify.app/,https://lesbonplan.vercel.app/,https://lesbonsplans-api.onrender.com/}")
     private String allowedOrigins;
    // @Value("${app.cors.allowed-origins}")
     // private List<String> allowedOrigins;
