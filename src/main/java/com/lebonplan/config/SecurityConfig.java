d843c65b3814e44940113cef87333c1db22231a9
maher moussa <maher.moussa90@yahoo.fr>
2026-05-16 21:39:18 +0200

    Update SecurityConfig.java

--- src/main/java/com/lebonplan/config/SecurityConfig.java
+++ src/main/java/com/lebonplan/config/SecurityConfig.java
@@ -39,10 +39,10 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final UserDetailsService userDetailsService;
 
-   // @Value("${app.cors.allowed-origins:http://localhost:5173,http://localhost:3000,https://lebonplan2.netlify.app/,https://lesbonsplans.netlify.app/}")
-    //private String allowedOrigins;
-    @Value("${app.cors.allowed-origins}")
-    private List<String> allowedOrigins;
+    @Value("${app.cors.allowed-origins:http://localhost:5173,http://localhost:3000,https://lebonplan2.netlify.app/,https://lesbonsplans.netlify.app/,https://lesbonplan.vercel.app/}")
+    private String allowedOrigins;
+   // @Value("${app.cors.allowed-origins}")
+    // private List<String> allowedOrigins;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
